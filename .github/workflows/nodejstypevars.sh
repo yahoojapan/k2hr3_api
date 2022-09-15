9879bf7641afb47dc25c92dda7ec7aa9398cff55
Takeshi Nakatani <nakatani@yahoo-corp.jp>
2022-09-15 12:31:02 +0900

    Cleaned up package.json and updated related package versions (#62)

--- .github/workflows/nodejstypevars.sh
+++ .github/workflows/nodejstypevars.sh
@@ -94,6 +94,12 @@ elif [ "X${NODE_MAJOR_VERSION}" = "X16" ]; then
 	INSTALL_QUIET_ARG="-qq"
 	PUBLISHER="true"
 
+elif [ "X${NODE_MAJOR_VERSION}" = "X18" ]; then
+	INSTALL_PKG_LIST="git gcc g++ make k2hdkc-dev"
+	INSTALLER_BIN="apt-get"
+	INSTALL_QUIET_ARG="-qq"
+	PUBLISHER="false"
+
 fi
 
 #
