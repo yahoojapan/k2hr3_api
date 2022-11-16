013654c86ad8d634884047076daa984cedac5c24
Takeshi Nakatani <ggtakec@gmail.com>
2022-11-16 14:52:06 +0900

    Updated github actions and nodejs version, and fixed bugs (#65)

--- tests/auto_template.sh
+++ tests/auto_template.sh
@@ -18,110 +18,109 @@
 # REVISION:
 #
 
-#
-# Common
-#
-PROGRAM_NAME=`basename $0`
-MYSCRIPTDIR=`dirname $0`
-MYSCRIPTDIR=`cd ${MYSCRIPTDIR}; pwd`
-SRCTOP=`cd ${MYSCRIPTDIR}/..; pwd`
+#==========================================================
+# Common Variables
+#==========================================================
+PRGNAME=$(basename "$0")
+SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(cd "${SCRIPTDIR}" || exit 1; pwd)
+SRCTOP=$(cd "${SCRIPTDIR}/.." || exit 1; pwd)
 
 #
-# Parameters
+# Variables
 #
-TEST_PROGRAM=${SRCTOP}/tests/k2hr3template_test.sh
-VARS_FILE=${SRCTOP}/tests/k2hr3template_test_vars.js
-TEMPL_FILE=${SRCTOP}/tests/k2hr3template_test_template.txt
-SUCCESS_FILE=${SRCTOP}/tests/k2hr3template_test_template.result
-EXPANDED_FILE=/tmp/k2hr3template_test_template.result
+TEST_PROGRAM="${SRCTOP}/tests/k2hr3template_test.sh"
+VARS_FILE="${SRCTOP}/tests/k2hr3template_test_vars.js"
+TEMPL_FILE="${SRCTOP}/tests/k2hr3template_test_template.txt"
+SUCCESS_FILE="${SRCTOP}/tests/k2hr3template_test_template.result"
+EXPANDED_FILE="/tmp/k2hr3template_test_template.result"
 
+#==============================================================
+# Utility functions
+#==============================================================
 #
 # Usage
 #
 PrintUsage()
 {
+	echo ""
 	echo "Usage: $1 [--debuglevel(-d) DBG/MSG/WARN/ERR/(custom debug level)] [--async(-a)]"
 	echo ""
 }
 
-#
-# Parse arguments
-#
+#==========================================================
+# Parse options
+#==========================================================
 DEBUG_OPTION=""
 DEBUG_ENV_DBG=0
 ASYNC_OPTION=""
 
 while [ $# -ne 0 ]; do
-	if [ "X$1" = "X" ]; then
+	if [ -z "$1" ]; then
 		break
 
-	elif [ "X$1" = "X--help" -o "X$1" = "X--HELP" -o "X$1" = "X-h" -o "X$1" = "X-H" ]; then
-		PrintUsage ${PROGRAM_NAME}
+	elif [ "$1" = "-h" ] || [ "$1" = "-H" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]; then
+		PrintUsage "${PRGNAME}"
 		exit 0
 
-	elif [ "X$1" = "X--debuglevel" -o "X$1" = "X--DEBUGLEVEL" -o "X$1" = "X-d" -o "X$1" = "X-D" ]; then
-		#
-		# DEBUG option
-		#
-		shift
-		if [ $# -eq 0 ]; then
-			echo "ERROR: --debuglevel(-d) option needs parameter(dbg/msg/warn/err)"
+	elif [ "$1" = "-d" ] || [ "$1" = "-D" ] || [ "$1" = "--debuglevel" ] || [ "$1" = "--DEBUGLEVEL" ]; then
+		if [ -n "${DEBUG_OPTION}" ]; then
+			echo "[ERROR] already specified --debuglevel(-d) option"
 			exit 1
 		fi
-		if [ "X$DEBUG_OPTION" != "X" ]; then
-			echo "ERROR: already specified  --debuglevel(-d) option"
+		shift
+		if [ $# -eq 0 ]; then
+			echo "[ERROR] --debuglevel(-d) option needs parameter(dbg/msg/warn/err)"
 			exit 1
 		fi
 
-		if [ "X$1" = "Xdbg" -o "X$1" = "XDBG" -o "X$1" = "Xdebug" -o "X$1" = "XDEBUG" ]; then
+		if [ "$1" = "dbg" ] || [ "$1" = "DBG" ] || [ "$1" = "debug" ] || [ "$1" = "DEBUG" ]; then
 			DEBUG_ENV_DBG=1
 		fi
 		DEBUG_OPTION="-d $1"
 
-	elif [ "X$1" = "X--async" -o "X$1" = "X--ASYNC" -o "X$1" = "X-a" -o "X$1" = "X-A" ]; then
-		if [ "X$ASYNC_OPTION" != "X" ]; then
-			echo "ERROR: already specified  --async(-a) option"
+	elif [ "$1" = "-a" ] || [ "$1" = "-A" ] || [ "$1" = "--async" ] || [ "$1" = "--ASYNC" ]; then
+		if [ -n "${ASYNC_OPTION}" ]; then
+			echo "[ERROR] already specified --async(-a) option"
 			exit 1
 		fi
 		ASYNC_OPTION="--async"
 
 	else
-		echo "ERROR: unknown option \"$1\""
-		echo ""
-		PrintUsage ${PROGRAM_NAME}
-		exit 0
+		echo "[ERROR] Unknown option $1"
+		exit 1
 	fi
-
 	shift
 done
 
-#
-# Executing
-#
-if [ ${DEBUG_ENV_DBG} -eq 1 ]; then
-	echo "***** Run(execute template engine) *****"
-	echo "${TEST_PROGRAM} -v ${VARS_FILE} -t ${TEMPL_FILE} ${ASYNC_OPTION} > ${EXPANDED_FILE}"
+#==========================================================
+# Do work
+#==========================================================
+if [ "${DEBUG_ENV_DBG}" -eq 1 ]; then
+	echo "[DEBUG] Run(execute template engine)"
+	echo "        ${TEST_PROGRAM} -v ${VARS_FILE} -t ${TEMPL_FILE} ${ASYNC_OPTION} > ${EXPANDED_FILE}"
 	echo ""
 fi
-${TEST_PROGRAM} -v ${VARS_FILE} -t ${TEMPL_FILE} ${ASYNC_OPTION} > ${EXPANDED_FILE}
-if [ $? -ne 0 ]; then
-	exit $?
+if ! /bin/sh -c "${TEST_PROGRAM} -v ${VARS_FILE} -t ${TEMPL_FILE} ${DEBUG_OPTION} ${ASYNC_OPTION} > ${EXPANDED_FILE}"; then
+	exit 1
 fi
 
-if [ ${DEBUG_ENV_DBG} -eq 1 ]; then
-	echo "***** Run(compare result) *****"
-	echo "diff ${EXPANDED_FILE} ${SUCCESS_FILE}"
+if [ "${DEBUG_ENV_DBG}" -eq 1 ]; then
+	echo "[DEBUG] Run(compare result)"
+	echo "        diff ${EXPANDED_FILE} ${SUCCESS_FILE}"
 	echo ""
 fi
-diff ${EXPANDED_FILE} ${SUCCESS_FILE}
-if [ $? -ne 0 ]; then
-	exit $?
+if ! diff "${EXPANDED_FILE}" "${SUCCESS_FILE}"; then
+	exit 1
 fi
 
 exit 0
 
 #
-# VIM modelines
-#
-# vim:set ts=4 fenc=utf-8:
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim600: noexpandtab sw=4 ts=4 fdm=marker
+# vim<600: noexpandtab sw=4 ts=4
 #
--- tests/run_local_test_k2hdkc.sh
+++ tests/run_local_test_k2hdkc.sh
@@ -18,137 +18,157 @@
 # REVISION:
 #
 
-#
-# Common
-#
-PROGRAM_NAME=`basename $0`
-MYSCRIPTDIR=`dirname $0`
-MYSCRIPTDIR=`cd ${MYSCRIPTDIR}; pwd`
-SRCTOP=`cd ${MYSCRIPTDIR}/..; pwd`
+#==========================================================
+# Common Variables
+#==========================================================
+PRGNAME=$(basename "$0")
+SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(cd "${SCRIPTDIR}" || exit 1; pwd)
+SRCTOP=$(cd "${SCRIPTDIR}/.." || exit 1; pwd)
+TESTDIR=$(cd "${SRCTOP}/tests" || exit 1; pwd)
 
 #
-# Parse arguments
+# Variables
 #
-IS_SCRIPT_MODE=0
-KEYWORD=""
+AUTO_CTRL_SH="${TESTDIR}/auto_control_subprocess.sh"
+TEST_LOAD_SH="${TESTDIR}/k2hdkc_test_load.sh"
+
+#==============================================================
+# Utility functions
+#==============================================================
+#
+# Usage
+#
+PrintUsage()
+{
+	echo ""
+	echo "Usage: $1 [--key(-k) keyword(group name for control)] [start | stop]"
+	echo ""
+}
+
+#==========================================================
+# Parse options
+#==========================================================
+SCRIPT_MODE=""
+KEY_SUFFIX=""
 
 while [ $# -ne 0 ]; do
-	if [ "X$1" = "X" ]; then
+	if [ -z "$1" ]; then
 		break
 
-	elif [ "X$1" = "X--help" -o "X$1" = "X--HELP" -o "X$1" = "X-h" -o "X$1" = "X-H" ]; then
-		echo "Usage:   ${PROGRAM_NAME} [--key(-k) keyword(group name for control)] [start | stop]"
+	elif [ "$1" = "-h" ] || [ "$1" = "-H" ] || [ "$1" = "--help" ] || [ "$1" = "--HELP" ]; then
+		PrintUsage "${PRGNAME}"
 		exit 0
 
-	elif [ "X$1" = "X--key" -o "X$1" = "X--KEY" -o "X$1" = "X-k" -o "X$1" = "X-K" ]; then
+	elif [ "$1" = "-k" ] || [ "$1" = "-K" ] || [ "$1" = "--key" ] || [ "$1" = "--KEY" ]; then
+		if [ -n "${KEY_SUFFIX}" ]; then
+			echo "[ERROR] already set --key(-k) option"
+			exit 1
+		fi
 		shift
 		if [ $# -eq 0 ]; then
-			echo "ERROR: --key(-k) option needs parameter"
+			echo "[ERROR] --key(-k) option needs parameter"
 			exit 1
 		fi
-		if [ "X${KEYWORD}" != "X" ]; then
-			echo "ERROR: already set --key(-k) option"
-			exit 1
-		fi
-		KEYWORD=_$1
+		KEY_SUFFIX="_$1"
 
-	elif [ "X$1" = "Xstart" -o "X$1" = "XSTART" ]; then
-		if [ ${IS_SCRIPT_MODE} -ne 0 ]; then
-			echo "ERROR: already specified start or stop"
+	elif [ "$1" = "start" ] || [ "$1" = "START" ]; then
+		if [ -n "${SCRIPT_MODE}" ]; then
+			echo "[ERROR] already specified start or stop"
 			exit 1
 		fi
-		IS_SCRIPT_MODE=1
+		SCRIPT_MODE="start"
 
-	elif [ "X$1" = "Xstop" -o "X$1" = "XSTOP" ]; then
-		if [ ${IS_SCRIPT_MODE} -ne 0 ]; then
-			echo "ERROR: already specified start or stop"
+	elif [ "$1" = "stop" ] || [ "$1" = "STOP" ]; then
+		if [ -n "${SCRIPT_MODE}" ]; then
+			echo "[ERROR] already specified start or stop"
 			exit 1
 		fi
-		IS_SCRIPT_MODE=2
+		SCRIPT_MODE="stop"
 
 	else
-		echo "ERROR: unknown option $1"
+		echo "[ERROR] Unknown option $1"
 		exit 1
 	fi
 	shift
 done
 
-if [ ${IS_SCRIPT_MODE} -eq 0 ]; then
-	echo "ERROR: must specify start or stop"
+#
+# Check option
+#
+if [ -z "${SCRIPT_MODE}" ]; then
+	echo "[ERROR] You must specify start or stop"
 	exit 1
 fi
 
-if [ ${IS_SCRIPT_MODE} -eq 1 ]; then
+#==========================================================
+# Do work
+#==========================================================
+if [ "${SCRIPT_MODE}" = "start" ]; then
 	#
 	# Start
 	#
 	echo "*** Start : chmpx server node for k2hdkc"
-	echo -n "            "
-	${SRCTOP}/tests/auto_control_subprocess.sh -start -key server${KEYWORD} -int 3 chmpx -conf ${SRCTOP}/tests/auto_k2hdkc_server.ini -ctlport 18021 -d msg
-	if [ $? -ne 0 ]; then
-		echo "ERROR: could not run chmpx server node for k2hdkc"
+    printf "           "
+	if ! "${AUTO_CTRL_SH}" --start --key "server${KEY_SUFFIX}" --interval 3 -- chmpx -conf "${TESTDIR}"/auto_k2hdkc_server.ini -ctlport 18021 -d msg; then
+		echo "[ERROR] could not run chmpx server node for k2hdkc"
 		exit 1
 	fi
 
 	echo "*** Start : one k2hdkc process"
-	echo -n "            "
-	${SRCTOP}/tests/auto_control_subprocess.sh -start -key server${KEYWORD} -int 3 k2hdkc -conf ${SRCTOP}/tests/auto_k2hdkc_server.ini -ctlport 18021 -d msg
-	if [ $? -ne 0 ]; then
-		echo "ERROR: one k2hdkc process"
+    printf "           "
+	if ! "${AUTO_CTRL_SH}" --start --key "server${KEY_SUFFIX}" --interval 3 -- k2hdkc -conf "${TESTDIR}"/auto_k2hdkc_server.ini -ctlport 18021 -d msg; then
+		echo "[ERROR] one k2hdkc process"
 		exit 1
 	fi
 
 	echo "*** Start : chmpx slave node for k2hdkc"
-	echo -n "            "
-	${SRCTOP}/tests/auto_control_subprocess.sh -start -key slave${KEYWORD} -int 3 chmpx -conf ${SRCTOP}/tests/auto_k2hdkc_slave.ini -ctlport 18031 -d msg
-	if [ $? -ne 0 ]; then
-		echo "ERROR: chmpx slave node for k2hdkc"
+    printf "           "
+	if ! "${AUTO_CTRL_SH}" --start --key "slave${KEY_SUFFIX}" --interval 3 -- chmpx -conf "${TESTDIR}"/auto_k2hdkc_slave.ini -ctlport 18031 -d msg; then
+		echo "[ERROR] chmpx slave node for k2hdkc"
 		exit 1
 	fi
 
 	echo "*** Start : load default test data to k2hdkc"
-	echo -n "            "
-	${SRCTOP}/tests/k2hdkc_test_load.sh --for_auto_test
-	if [ $? -ne 0 ]; then
-		echo "ERROR: load default test data to k2hdkc"
+    printf "           "
+	if ! "${TEST_LOAD_SH}" --for_auto_test; then
+		echo "[ERROR] load default test data to k2hdkc"
 		exit 1
 	fi
-
 else
 	#
 	# Stop
 	#
 	echo "*** Stop : chmpx slave node for k2hdkc"
-	echo -n "           "
-	${SRCTOP}/tests/auto_control_subprocess.sh -stop -key slave${KEYWORD} -int 3 chmpx
-	if [ $? -ne 0 ]; then
-		echo "ERROR: chmpx slave node for k2hdkc"
+    printf "           "
+	if ! "${AUTO_CTRL_SH}" --stop --key "slave${KEY_SUFFIX}" --interval 3 -- chmpx; then
+		echo "[ERROR] chmpx slave node for k2hdkc"
 		exit 1
 	fi
 
 	echo "*** Stop : one k2hdkc process"
-	echo -n "           "
-	${SRCTOP}/tests/auto_control_subprocess.sh -stop -key server${KEYWORD} -int 3 k2hdkc
-	if [ $? -ne 0 ]; then
-		echo "ERROR: one k2hdkc process"
+    printf "           "
+	if ! "${AUTO_CTRL_SH}" --stop --key "server${KEY_SUFFIX}" --interval 3 -- k2hdkc; then
+		echo "[ERROR] one k2hdkc process"
 		exit 1
 	fi
 
 	echo "*** Stop : chmpx server node for k2hdkc"
-	echo -n "           "
-	${SRCTOP}/tests/auto_control_subprocess.sh -stop -key server${KEYWORD} -int 3 chmpx
-	if [ $? -ne 0 ]; then
-		echo "ERROR: could not run chmpx server node for k2hdkc"
+    printf "           "
+	if ! "${AUTO_CTRL_SH}" --stop --key "server${KEY_SUFFIX}" --interval 3 -- chmpx; then
+		echo "[ERROR] could not run chmpx server node for k2hdkc"
 		exit 1
 	fi
 fi
+echo "[SUCCEED] Finished"
 
-echo ""
-echo "SUCCEED: finished"
 exit 0
 
 #
-# VIM modelines
-#
-# vim:set ts=4 fenc=utf-8:
+# Local variables:
+# tab-width: 4
+# c-basic-offset: 4
+# End:
+# vim600: noexpandtab sw=4 ts=4 fdm=marker
+# vim<600: noexpandtab sw=4 ts=4
 #
